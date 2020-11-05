86929779ce67225a13d6cab407261027462bc400
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2020-11-05 20:34:15 +0300

    Third lesson

---
+++ UnityProject/IntroductionInUnity/Assets/MyScripts/PlayerStates/GroundedState.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+
+namespace EscapeRoom {
+
+    public class GroundedState : State {
+
+        #region Fields
+
+        protected float _speed;
+        protected float _rotationSpeed;
+
+        private float _horizontalInput;
+        private float _verticalInput;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public GroundedState(Player player, StateMachine stateMachine) : base(player, stateMachine) {}
+
+        #endregion
+
+
+        #region Methods
+
+        public override void Enter() {
+            base.Enter();
+
+            _horizontalInput = _verticalInput = 0.0f;
+        }
+
+        public override void Exit() {
+            base.Exit();
+
+            _player.ResetMoveParams();
+        }
+
+        public override void HandleInput() {
+            base.HandleInput();
+
+            _verticalInput = Input.GetAxis("Vertical");
+            _horizontalInput = Input.GetAxis("Horizontal");
+        }
+
+        public override void PhysicsUpdate() {
+            base.PhysicsUpdate();
+
+            _player.Move(_verticalInput * _speed, _horizontalInput * _rotationSpeed);
+        }
+
+        #endregion
+    }
+}
---
+++ UnityProject/IntroductionInUnity/Assets/Resourses/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Game Data/Player Data")] public class PlayerData : ScriptableObject {
+
+    #region Fields
+
+    public float MovementSpeed = 150.0f;
+    public float CrouchSpeed = 50.0f;
+    public float RunSpeed = 200.0f;
+
+    public float CrouchColliderHeight = 1.0f;
+    public float NormalColliderHeight = 2.0f;
+
+    public float RotationSpeed = 90.0f;
+    public float RunRotationSpeed = 60.0f;
+    public float CrouchRotationSpeed = 45.0f;
+
+    public float JumpForce = 5.0f;
+    public float RunJumpForce = 7.5f;
+
+    #endregion
+}
