e9c7f5687f089f3c808e95a11dabd0926d9759f6
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2020-11-13 16:15:02 +0300

    Lesson 2: Create first game scene (#4)
    * Submission for second lesson
    
    * black magic

---
+++ UnityProject/IntroductionInUnity/Assets/MyScripts/Movement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace EscapeRoom {
+
+    public class Movement : MonoBehaviour {
+
+        #region Fields
+
+        [SerializeField] private float _speed = 5.0F;
+        [SerializeField] private float _rotation = 10.0F;
+
+        private Rigidbody _rigidbody;
+
+        private Vector3 _direction;
+
+        #endregion
+
+        #region UnityMethods
+
+        private void Start() {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update() {
+            _direction.z = -Input.GetAxis("Horizontal"); // <- minus for Black Magic
+            _direction.x = Input.GetAxis("Vertical");
+
+            _direction.Normalize();
+
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _direction, _rotation * Time.deltaTime, 0f);
+            transform.rotation = Quaternion.LookRotation(desiredForward);
+        }
+
+
+        private void FixedUpdate() {
+            var speed = (_direction.sqrMagnitude > 0) ? _speed : 0;
+            speed = speed * 10.0F * Time.fixedDeltaTime;
+
+            var moveDirection = transform.forward * speed;
+            _rigidbody.velocity = moveDirection;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void Move() {
+            Vector3 targetVelocity = _speed * transform.forward * Time.deltaTime;
+            targetVelocity.y = GetComponent<Rigidbody>().velocity.y;
+
+            GetComponent<Rigidbody>().velocity = targetVelocity;
+            GetComponent<Rigidbody>().angularVelocity = _rotation * Vector3.up * Time.deltaTime;
+        }
+
+        #endregion
+    }
+}
