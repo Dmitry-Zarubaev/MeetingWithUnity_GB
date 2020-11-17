27d4905ac8fe35feced8c5cd897acb73d46a0e81
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2020-11-17 22:57:09 +0300

    Basic UI

---
+++ UnityProject/IntroductionInUnity/Assets/MyScripts/Fire.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace EscapeRoom {
+
+    public class Fire : MonoBehaviour {
+
+        private CapsuleCollider _collider;
+
+        private void Start() {
+            _collider = GetComponent<CapsuleCollider>();
+        }
+    }
+}
+
